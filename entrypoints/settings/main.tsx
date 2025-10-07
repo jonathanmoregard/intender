aba0356d0b94881e10bd115eb4e4c920347c510a
Jonathan Moregård <jonathan.more@hotmail.com>
2025-10-07 19:33:30 +0200

    Update main.tsx

--- entrypoints/settings/main.tsx
+++ entrypoints/settings/main.tsx
@@ -814,21 +814,7 @@ const SettingsTab = memo(
             onClick={addIntention}
             title='Add another intention'
           >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='16'
-              height='16'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeWidth='3'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              aria-hidden='true'
-            >
-              <line x1='12' y1='5' x2='12' y2='19'></line>
-              <line x1='5' y1='12' x2='19' y2='12'></line>
-            </svg>
+           Add Intention
           </button>
           <button className='save-btn' onClick={update}>
             Save Changes
